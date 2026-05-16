3ae370ab6420c2e0b2f88d5badedff3bb650f4e5
avnsk <ananay@Ananays-MacBook-Air.local>
2026-05-16 09:28:09 +0100

    Adding method for computing with 10 threads

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::Arc;
+use std::thread;
 use std::time::Instant;
+
 static COUNT: AtomicU64 = AtomicU64::new(0);
 
 fn is_prime(n: u64) {
@@ -18,6 +21,27 @@ fn is_prime(n: u64) {
     COUNT.fetch_add(1, Ordering::SeqCst);
 }
 
+fn multi_thread_counting(range_limit: u64) {
+    let num_threads = 10;
+    let next_number = Arc::new(AtomicU64::new(1));
+    let start_time = Instant::now();
+    let mut handles = vec![];
+    for _ in 0..num_threads {
+        let next_number = Arc::clone(&next_number);
+        let handle = thread::spawn(move || loop {
+            let n = next_number.fetch_add(1, Ordering::SeqCst);
+            if n > range_limit {
+                break;
+            }
+            is_prime(n);
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
 fn count_single_thread(range_limit: u64) {
     for n in 1..=range_limit {
         is_prime(n);
@@ -27,7 +51,8 @@ fn count_single_thread(range_limit: u64) {
 fn main() {
     let range_limit = 100000000;
     let start_time = Instant::now();
-    count_single_thread(range_limit);
+    //count_single_thread(range_limit);
+    multi_thread_counting(range_limit);
     let final_count = COUNT.load(Ordering::SeqCst);
     let duration = start_time.elapsed();
     println!(
